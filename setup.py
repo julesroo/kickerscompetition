b16118f6de03726aa3a8fbcb6fecf7fbd9bc8a4b
Jules Roovers <julesroovers@gmail.com>
2017-07-15 18:31:33 +0200

    Tafelvoetbal competitie V1

---
+++ setup.py
@@ -0,0 +1,92 @@
+# TAFELVOETBAL COMPETITIE - ANGRY BIRDS KPN
+# Jules Roovers, July 2017
+# Use this file for initial setup
+
+
+import random
+import itertools
+import pandas as pd
+
+
+def setup():
+    players = ["Harry",
+               "Wing",
+               "Vincent",
+               "Stephan",
+               "Hesing",
+               "Mathieu",
+               "Jules",
+               "Gast"]
+
+    idlist = []
+    for i in range(0, len(players)):
+        idlist.append(i)
+
+    zero = []
+    for i in range(0, len(players)):
+        zero.append(0)
+
+    dataframe = pd.DataFrame({'ID': idlist,
+                              'Spelers': players,
+                              'Aantal gespeeld': zero,
+                              'Aantal gewonnen': zero})
+
+    df = dataframe[['ID', 'Spelers', 'Aantal gespeeld', 'Aantal gewonnen']]
+
+    print(df)
+
+    df.to_csv('data.csv', index=False, sep=';')
+
+
+def schedule(players):
+    set_size = 2
+    schedule = set()
+    teams = range(len(players))
+
+    for comb in itertools.product(teams, repeat=set_size):
+        comb = sorted(list(comb))
+        if len(set(comb)) == set_size:
+            schedule.add(tuple(comb))
+
+    schedule = list(schedule)
+    random.shuffle(schedule)
+
+    sorted_schedule = []
+    for i in schedule:
+        sorted_schedule.append(sorted(i))
+
+    team_one = []
+    team_two = []
+    zero = []
+
+
+    for subset in itertools.combinations(sorted_schedule, 2):
+        if not any(map(lambda v: v in subset[0], subset[1])):
+            one = str(subset[0]).translate(None, " []")
+            two = str(subset[1]).translate(None, " []")
+
+            team_one.append(one)
+            team_two.append(two)
+
+
+    for i in range(0, len(team_one)):
+        zero.append(0)
+
+    subset_df = pd.DataFrame({'Team_one': team_one,
+                              'Team_two': team_two,
+                              'Gespeeld': zero,
+                              'Gewonnen_one': zero,
+                              'Gewonnen_two': zero})
+    subset_df.to_csv('matches.csv', sep=';')
+
+    return sorted_schedule
+
+
+setup()
+
+players_df = pd.read_csv('data.csv', sep=';', index_col='ID')
+players = players_df['Spelers'].values.tolist()
+
+schedule(players)
+
+print('Setup complete!')
--- tournament.py
+++ tournament.py
@@ -3,45 +3,39 @@
 # Enjoy!
 
 import random
-import itertools
 import pandas as pd
-
-def schedule(players):
-    set_size = 2
-    schedule = set()
-    teams = range(len(players))
-
-    for comb in itertools.product(teams, repeat=set_size):
-        comb = sorted(list(comb))
-        if len(set(comb)) == set_size:
-            schedule.add(tuple(comb))
-
-    schedule = list(schedule)
-    random.shuffle(schedule)
-
-    sorted_schedule = []
-    for i in schedule:
-        sorted_schedule.append(sorted(i))
-
-    return sorted_schedule
+import csv
+import linecache
 
 
-def new_round(schema):
-    team_one = []
-    team_two = []
+def new_round():
+    with open('matches.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=';')
+        data = list(reader)
+        row_count = len(data)
+        offset = random.randrange(2, row_count)
+        random_line = linecache.getline("matches.csv", offset)
 
+        random_line_strip = random_line.rstrip()
 
-    for i in range(2):
-       for f in (random.choice(schema)):
-            if i == 0:
-                team_one.append(players_df.loc[f, 'Spelers'])
-            else:
-                team_two.append(players_df.loc[f, 'Spelers'])
+        items = random_line_strip.split(';')
+        team_one = list(items[4])
+        t1 = [x for x in team_one if x != ',']
+        t1_int = []
+        team_two = list(items[5])
+        t2 = [x for x in team_two if x != ',']
+        t2_int = []
 
-    if any(map(lambda v: v in team_one, team_two)):
-        return new_round(schedule(players))
+        for item in t1:
+            t1_int.append(int(item))
 
-    x = [team_one, team_two]
+        for item in t2:
+            t2_int.append(int(item))
+
+    if items[1] == 1:
+        return new_round()
+
+    x = [t1_int, t2_int]
 
     return x
 
@@ -52,29 +46,45 @@ def match(schema):
     one = schema[0]
     two = schema[1]
 
+    t1 = str(schema[0])
+    t1_str = t1.translate(None, " []")
+    t2 = str(schema[1])
+    t2_str = t2.translate(None, " []")
+
+    team_one = []
+    team_two = []
+
+    for i in one:
+        team_one.append(players_df.loc[int(i), 'Spelers'])
+    for p in two:
+        team_two.append(players_df.loc[int(p), 'Spelers'])
+
     print "De volgende teams spelen tegen elkaar: \n "
-    print "Team 1: " + str(one)
-    print "Team 2: " + str(two)
+    print "Team 1: " + str(team_one)
+    print "Team 2: " + str(team_two)
 
     while play == "n":
         input_play = (raw_input("Met deze teams spelen? (Y/N) \n")).lower()
 
         if input_play == "n":
-            play = match(new_round(schedule(players)))
+            play = match(new_round())
         elif input_play == "y":
             play = "y"
-            match_df.loc[one[0], 'Aantal gespeeld'] += 1
-            match_df.loc[one[1], 'Aantal gespeeld'] += 1
-            match_df.loc[two[0], 'Aantal gespeeld'] += 1
-            match_df.loc[two[1], 'Aantal gespeeld'] += 1
+            match_df.loc[team_one[0], 'Aantal gespeeld'] += 1
+            match_df.loc[team_one[1], 'Aantal gespeeld'] += 1
+            match_df.loc[team_two[0], 'Aantal gespeeld'] += 1
+            match_df.loc[team_two[1], 'Aantal gespeeld'] += 1
 
-            match_df.to_csv('data.csv', sep = ';')
+            match_df.to_csv('data.csv', sep=';')
+
+            played_df.loc[(played_df['Team_one'].str.contains(t1_str) == True) & (
+                played_df['Team_two'].str.contains(t2_str) == True), 'Gespeeld'] += 1
+            played_df.to_csv('matches.csv', sep=';', index=None)
 
             break
         else:
             play = "n"
             print "Geen geldig antwoord. Gebruik Y of N.\n"
-
 
     x = [one, two]
 
@@ -82,11 +92,22 @@ def match(schema):
 
 
 def play(teams):
-    # for player in teams:
-    #    games_played[player] += 1
 
     one = teams[0]
     two = teams[1]
+
+    t1 = str(teams[0])
+    t1_str = t1.translate(None, " []")
+    t2 = str(teams[1])
+    t2_str = t2.translate(None, " []")
+
+    team_one = []
+    team_two = []
+
+    for i in one:
+        team_one.append(players_df.loc[int(i), 'Spelers'])
+    for p in two:
+        team_two.append(players_df.loc[int(p), 'Spelers'])
 
     winner = "n"
 
@@ -94,79 +115,42 @@ def play(teams):
         input_winner = (raw_input("Welk team heeft gewonnen? (1/2) \n")).lower()
 
         if input_winner == "1":
-            print('De winnaars zijn ' + str(one[0]) + ' en ' + str(one[1]) + '!')
+            print('De winnaars zijn ' + str(team_one[0]) + ' en ' + str(team_one[1]) + '!')
             winner = "y"
-            match_df.loc[one[0], 'Aantal gewonnen'] += 1
-            match_df.loc[one[1], 'Aantal gewonnen'] += 1
+            match_df.loc[team_one[0], 'Aantal gewonnen'] += 1
+            match_df.loc[team_one[1], 'Aantal gewonnen'] += 1
+
+            played_df.loc[(played_df['Team_one'].str.contains(t1_str) == True) & (
+                played_df['Team_two'].str.contains(t2_str) == True), 'Gewonnen_one'] += 1
+            played_df.to_csv('matches.csv', sep=';', index=None)
+
         elif input_winner == "2":
             print('De winnaars zijn ' + str(two[0]) + ' en ' + str(two[1]) + '!')
-            winner ="y"
-            match_df.loc[two[0], 'Aantal gewonnen'] += 1
-            match_df.loc[two[1], 'Aantal gewonnen'] += 1
+            winner = "y"
+            match_df.loc[team_two[0], 'Aantal gewonnen'] += 1
+            match_df.loc[team_two[1], 'Aantal gewonnen'] += 1
+
+            played_df.loc[(played_df['Team_one'].str.contains(t1_str) == True) & (
+                played_df['Team_two'].str.contains(t2_str) == True), 'Gewonnen_two'] += 1
+            played_df.to_csv('matches.csv', sep=';', index=None)
+
         else:
             winner = "n"
             print "Geen geldig antwoord. Gebruik 1 of 2.\n"
 
     match_df.to_csv('data.csv', sep=';')
 
-def setup():
-    players = ["Harry",
-                "Wing",
-                "Vincent",
-                "Stephan",
-                "Hesing",
-                "Mathieu",
-                "Jules",
-                "Gast"]
-
-
-    idlist = []
-    for i in range(0, len(players)):
-        idlist.append(i)
-
-    zero = []
-    for i in range(0, len(players)):
-        zero.append(0)
-
-    dataframe = pd.DataFrame({'ID': idlist,
-                            'Spelers': players,
-                            'Aantal gespeeld': zero,
-                            'Aantal gewonnen': zero})
-
-    df = dataframe[['ID', 'Spelers', 'Aantal gespeeld', 'Aantal gewonnen']]
-
-    print(df)
-
-    df.to_csv('data.csv', index=False, sep=';')
-
-    print('Setup complete!')
-
-
-
-
 
 print ("--------------------------------------- \n")
-print ("TAFEL COMPETITIE 2017 - ANGRY BIRDS KPN \n")
+print ("TAFELVOETBAL COMPETITIE 2017 - ANGRY BIRDS KPN \n")
 print ("--------------------------------------- \n")
-players_df = pd.read_csv('data.csv', sep=';', index_col='ID')
+
 match_df = pd.read_csv('data.csv', sep=';', index_col='Spelers')
+players_df = pd.read_csv('data.csv', sep=';', index_col='ID')
+played_df = pd.read_csv('matches.csv', sep=';')
 
-players = players_df['Spelers'].values.tolist()
+play(match(new_round()))
 
+match_df_sel = match_df[['Aantal gespeeld', 'Aantal gewonnen']]
 
-play(match(new_round(schedule(players))))
-
-print(match_df.sort_values('Aantal gewonnen', ascending=False))
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(match_df_sel.sort_values('Aantal gewonnen', ascending=False))
